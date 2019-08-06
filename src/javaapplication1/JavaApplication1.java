77f5caf0aa3536f7a1083b145b3fef0f48a41656
mdwasim15596 <mdwasim15596@gmail.com>
2019-08-06 16:01:59 +0530

    first commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author ahmad
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    scanner sc=new scanner();
+    public static void main(String[] args) {
+        
+       int a,b;
+       System.out.println("\n  enter no a");
+      
+        // TODO code application logic here
+    }
+    
+}
